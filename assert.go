ffe6a3a84accb0cd71f2cc9e1fcb9c68bdef213c
Marco Molteni <marco.molteni@mailbox.org>
2024-09-02 21:47:16 +0200

    add AssertPanicTextEq

--- assert.go
+++ assert.go
@@ -2,6 +2,7 @@ package rosina
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -38,11 +39,33 @@ func AssertErrorIs(t *testing.T, err error, want error) {
 func AssertErrorTextEq(t *testing.T, err error, want string) {
 	t.Helper()
 	if err == nil {
-		t.Fatalf("\nhave: <no error>\nwant: %s", want)
+		t.Fatalf("\nhave: <no error>\nwant error: %s", want)
 	}
 	if delta := diff(err.Error(), want); delta != "" {
 		t.Fatalf("\n%s mismatch: +have -want:\n%s", "error text mismatch", delta)
 	}
+}
+
+func AssertPanicTextEq(t *testing.T, fn func(), want string) {
+	t.Helper()
+
+	var recovered any
+
+	// This function wrapper is needed to make t.Helper() report the
+	// correct file after the panic is recovered.
+	func() {
+		defer func() {
+			recovered = recover()
+		}()
+
+		fn()
+	}()
+
+	if recovered == nil {
+		t.Fatalf("\nhave: <no panic>\nwant panic: %s", want)
+	}
+	msg := fmt.Sprint(recovered)
+	AssertEqual(t, msg, want, "panic message")
 }
 
 // diff returns a textual representation of the differences between 'have' and
